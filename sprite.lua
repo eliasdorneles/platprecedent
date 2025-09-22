946caae80332293fca0139ce25dc90883672635b
Elias Dorneles <elias.dorneles@hey.com>
2025-09-22 20:12:49 +0200

    sync: update sprite.lua

--- sprite.lua
+++ sprite.lua
@@ -54,4 +54,93 @@ function M.Group:draw()
     end
 end
 
+M.Registry = {}
+
+function M.Registry:new()
+    self.__index = self
+    return setmetatable({
+        groups = {}
+    }, self)
+end
+
+function M.Registry:getGroup(name)
+    if not self.groups[name] then
+        self.groups[name] = M.Group:new()
+    end
+    return self.groups[name]
+end
+
+function M.Registry:_check_group(name)
+    if not self.groups[name] then
+        error(string.format("Group not found in registry: '%s'", name))
+    end
+end
+
+function M.Registry:groupiter(name)
+    return self:getGroup(name):iter()
+end
+
+function M.Registry:add(name, sprite)
+    self:getGroup(name):add(sprite)
+end
+
+function M.Registry:addAll(name, sprites_to_add)
+    self:getGroup(name):addAll(sprites_to_add)
+end
+
+function M.Registry:iteritems()
+    local i = 0
+    local k, _ = next(self.groups)
+    return function()
+        if k then
+            i = i + 1
+            if i <= #self.groups[k].sprites then
+                return k, self.groups[k].sprites[i]
+            end
+            -- next group:
+            i = 1
+            k, _ = next(self.groups, k)
+            if k and i <= #self.groups[k].sprites then
+                return k, self.groups[k].sprites[i]
+            end
+        end
+    end
+end
+
+function M.Registry:update(dt)
+    for _, group in pairs(self.groups) do
+        group:update(dt)
+    end
+end
+
+function M.Registry:cleanup(name)
+    if name then
+        self:getGroup(name):cleanup()
+    else
+        for _, group in pairs(self.groups) do
+            group:cleanup()
+        end
+    end
+end
+
+function M.Registry:killall(name)
+    if name then
+        self:getGroup(name):killall()
+    else
+        for _, group in pairs(self.groups) do
+            group:killall()
+        end
+    end
+end
+
+function M.Registry:draw(name)
+    if name then
+        self:getGroup(name):draw()
+    else
+        for _, group in pairs(self.groups) do
+            group:draw()
+        end
+    end
+end
+
 return M
